a2df1e7746cc9bdf5b58de40809695a1379706a5
Suit9uy <129301387+Suit9uy@users.noreply.github.com>
2025-01-19 12:19:28 +0800

    fix type error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import { Bounded } from "./Bounded";
 import { FooterPhysics } from "./FooterPhysics";
 import { asImageSrc } from "@prismicio/client";
 
-export async function Footer({ }: Props) {
+export async function Footer() {
   const client = createClient();
   const settings = await client.getSingle("settings");
 
